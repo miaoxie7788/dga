f04951779555016d4830dcb02bc865691f33720f
Miao Xie <miao.xie@team.telstra.com>
2019-11-05 00:42:23 +1100

    Add the main python file dga.py.

---
+++ dga.py
@@ -0,0 +1,23 @@
+
+def ingest():
+    pass
+
+
+def transform():
+    pass
+
+
+def train():
+    pass
+
+
+def predict():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
