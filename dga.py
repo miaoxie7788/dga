68d954c348a747194b7f5fc00acf2a9958f7fde0
Miao Xie <miao.xie@team.telstra.com>
2019-11-05 16:06:08 +1100

    Add feature extraction into function transform().

--- dga.py
+++ dga.py
@@ -1,5 +1,8 @@
 import pandas as pd
 from params import csv_params
+from udfs import transform_domain_len_udf, transform_domain_num_len_udf, \
+    transform_domain_sym_len_udf, transform_domain_vow_len_udf, transform_domain_uniq_count_udf, \
+    transform_domain_norm_ent_udf, transform_domain_gini_inx_udf, transform_domain_class_err_udf
 
 
 def ingest(params):
@@ -28,9 +31,18 @@ def describe(df):
         print("{column} has {n} unique values.".format(column=column, n=df[column].nunique()))
 
 
+def transform(df):
 
-def transform():
-    pass
+    df = df.assign(t_f1=df['domain'].apply(transform_domain_len_udf))
+    df = df.assign(t_f2=df['domain'].apply(transform_domain_num_len_udf))
+    df = df.assign(t_f3=df['domain'].apply(transform_domain_sym_len_udf))
+    df = df.assign(t_f4=df['domain'].apply(transform_domain_vow_len_udf))
+    df = df.assign(t_f5=df['domain'].apply(transform_domain_uniq_count_udf))
+    df = df.assign(t_f6=df['domain'].apply(transform_domain_norm_ent_udf))
+    df = df.assign(t_f7=df['domain'].apply(transform_domain_gini_inx_udf))
+    df = df.assign(t_f8=df['domain'].apply(transform_domain_class_err_udf))
+
+    return df
 
 
 def train():
@@ -44,6 +56,11 @@ def predict():
 def main():
     df = ingest(csv_params)
     describe(df)
+
+    df = transform(df)
+
+    print(df)
+
 
 
 if __name__ == "__main__":
